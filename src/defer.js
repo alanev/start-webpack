fcd7ba9faa5d70f759e0c18f3d125297687f8893
alanevEugene <e@alanev.ru>
2016-09-29 22:27:03 +0300

    Initial commit

---
+++ src/defer.js
@@ -0,0 +1,12 @@
+
+// Pages
+import './index.html'
+
+// Styles
+import './styles.scss'
+
+// Images
+import '../modules/a/a.png'
+
+// Scripts
+import '../modules/a/a.js'
---
+++ test/tasks.js
@@ -0,0 +1,42 @@
+import test from 'ava'
+import stat from '../utils/stat'
+import exec from '../utils/exec'
+
+const tasks = [
+	{
+		name: 'build',
+		output:
+		[
+			'index.html',
+			'styles.css',
+			'defer.js',
+			'assets/images/a.png',
+			'assets/images/a.webp'
+		]
+	}
+]
+
+test.before('remove build dir', () => {
+	return exec('rm -rf ../build/')
+		.then(() => {
+			console.log('Build folder is removed')
+		})
+})
+
+tasks.forEach(({ name, output }) => {
+	test(
+		`${name} task`,
+		t => exec(`npm run ${name}`)
+			.then(() => {
+				return Promise
+					.all(output.map(path => stat(`../build/${path}`)))
+					.then(() => {
+						t.pass()
+					})
+					.catch(({ path }) => {
+						t.fail(`Err: ${path} doesn't exist`)
+					})
+			})
+			.catch(err => t.fail(`Err: ${err}`))
+	)
+})
